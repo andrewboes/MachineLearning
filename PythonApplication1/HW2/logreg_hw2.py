6d282b63ea0479722aca29ef1b391598c9a4099b
Andrew <andrew.boes@gmail.com>
2021-10-18 18:29:08 -0700

    Implemented the first few questions.
    execution time is about 0.007 for each neg log like
    for 1000 7.315700054168701

--- PythonApplication1/HW2/logreg_hw2.py
+++ PythonApplication1/HW2/logreg_hw2.py
@@ -1,15 +1,15 @@
 import numpy as np
+import time
 np.random.seed(42)
 import matplotlib.pyplot as plt
 import logging
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 # GLOBAL PARAMETERS FOR STOCHASTIC GRADIENT DESCENT
-step_size=0.0001
-max_iters=1000
+step_size = 0.0001
+max_iters = 1000
 
 def main():
 
@@ -22,10 +22,10 @@ def main():
   # Fit a logistic regression model on train and plot its losses
   logging.info("Training logistic regression model (No Bias Term)")
   w, losses = trainLogistic(X_train,y_train)
-  y_pred_train = X_train@w >= 0
+  y_pred_train = X_train @ w >= 0
   
   logging.info("Learned weight vector: {}".format([np.round(a,4)[0] for a in w]))
-  logging.info("Train accuracy: {:.4}%".format(np.mean(y_pred_train == y_train)*100))
+  logging.info("Train accuracy: {:.4}%".format(np.mean(y_pred_train == y_train) * 100))
   
   logging.info("\n---------------------------------------------------------------------------\n")
 
@@ -34,10 +34,10 @@ def main():
   # Fit a logistic regression model on train and plot its losses
   logging.info("Training logistic regression model (Added Bias Term)")
   w, bias_losses = trainLogistic(X_train_bias,y_train)
-  y_pred_train = X_train_bias@w >= 0
+  y_pred_train = X_train_bias @ w >= 0
   
   logging.info("Learned weight vector: {}".format([np.round(a,4)[0] for a in w]))
-  logging.info("Train accuracy: {:.4}%".format(np.mean(y_pred_train == y_train)*100))
+  logging.info("Train accuracy: {:.4}%".format(np.mean(y_pred_train == y_train) * 100))
 
 
   plt.figure(figsize=(16,9))
@@ -56,7 +56,7 @@ def main():
   # Perform k-fold cross
   for k in [2,3,4, 5, 10, 20, 50]:
     cv_acc, cv_std = kFoldCrossVal(X_train_bias, y_train, k)
-    logging.info("{}-fold Cross Val Accuracy -- Mean (stdev): {:.4}% ({:.4}%)".format(k,cv_acc*100, cv_std*100))
+    logging.info("{}-fold Cross Val Accuracy -- Mean (stdev): {:.4}% ({:.4}%)".format(k,cv_acc * 100, cv_std * 100))
 
   ####################################################
   # Write the code to make your test submission here
@@ -67,100 +67,109 @@ def main():
 
 
 ######################################################################
-# Q3.1 logistic 
+# Q3.1 logistic
 ######################################################################
 # Given an input vector z, return a vector of the outputs of a logistic
 # function applied to each input value
 #
-# Input: 
-#   z --   a n-by-1 vector
+# Input:
+#   z -- a n-by-1 vector
 #
 # Output:
-#   logit_z --  a n-by-1 vector where logit_z[i] is the result of 
+#   logit_z -- a n-by-1 vector where logit_z[i] is the result of
 #               applying the logistic function to z[i]
 ######################################################################
 def logistic(z):
-  raise Exception('Student error: You haven\'t implemented the logistic calculation yet.')
-  return logit_z
+    ##sigma(z) = 1/(1+e^(w*X))
+    return (1 / (1 + np.exp(-z))).T
 
 
 ######################################################################
-# Q3.2 calculateNegativeLogLikelihood 
+# Q3.2 calculateNegativeLogLikelihood
 ######################################################################
 # Given an input data matrix X, label vector y, and weight vector w
 # compute the negative log likelihood of a logistic regression model
 # using w on the data defined by X and y
 #
-# Input: 
-#   X --   a n-by-d matrix of examples where each row
+# Input:
+#   X -- a n-by-d matrix of examples where each row
 #                   corresponds to a single d-dimensional example
 #
-#   y --    a n-by-1 vector representing the labels of the examples in X
+#   y -- a n-by-1 vector representing the labels of the examples in X
 #
-#   w --    a d-by-1 weight bector
+#   w -- a d-by-1 weight bector
 #
 # Output:
-#   nll --  the value of the negative log-likelihood
+#   nll -- the value of the negative log-likelihood
 ######################################################################
-def calculateNegativeLogLikelihood(X,y,w):
-  raise Exception('Student error: You haven\'t implemented the negative log likelihood calculation yet.')
-  return nll
-
-
+def calculateNegativeLogLikelihood(X,y,w):    
+    logLikelihood = 0
+    for i,x in enumerate(X):
+        len(x)
+        logisiticOfx = logistic(w.T @ x)
+        logLikelihood += y[i] * np.log(logisiticOfx) + (1 - y[i]) * np.log(1 - logisiticOfx)
+    return -logLikelihood
 
 
 ######################################################################
 # Q4 trainLogistic
 ######################################################################
-# Given an input data matrix X, label vector y, maximum number of 
-# iterations max_iters, and step size step_size -- run max_iters of 
+# Given an input data matrix X, label vector y, maximum number of
+# iterations max_iters, and step size step_size -- run max_iters of
 # gradient descent with a step size of step_size to optimize a weight
 # vector that minimizies negative log-likelihood on the data defined
 # by X and y
 #
-# Input: 
-#   X --   a n-by-d matrix of examples where each row
+# Input:
+#   X -- a n-by-d matrix of examples where each row
 #                   corresponds to a single d-dimensional example
 #
-#   y --    a n-by-1 vector representing the labels of the examples in X
+#   y -- a n-by-1 vector representing the labels of the examples in X
 #
-#   max_iters --   the maximum number of gradient descent iterations
+#   max_iters -- the maximum number of gradient descent iterations
 #
 #   step_size -- the step size (or learning rate) for gradient descent
 #
 # Output:
-#   w --  the d-by-1 weight vector at the end of training
+#   w -- the d-by-1 weight vector at the end of training
 #
 #   losses -- a list of negative log-likelihood values for each iteration
 ######################################################################
 def trainLogistic(X,y, max_iters=max_iters, step_size=step_size):
+    
 
     # Initialize our weights with zeros
-    w = np.zeros( (X.shape[1],1) )
+    w = np.zeros((X.shape[1],1))
     
     # Keep track of losses for plotting
     losses = [calculateNegativeLogLikelihood(X,y,w)]
-    
+    t2 = time.time()
     # Take up to max_iters steps of gradient descent
     for i in range(max_iters):
-    
+        
+        t0 = time.time()
                
         # Todo: Compute the gradient over the dataset and store in w_grad
         # .
-        # . Implement equation 9.
+        # .  Implement equation 9.
         # .
-     
-        raise Exception('Student error: You haven\'t implemented the gradient calculation for trainLogistic yet.')
+        gradientW = np.zeros((X.shape[1],1))
+        
+        for i, x in enumerate(X):
+            gw = (logistic(w.T @ x) - y[i]) * x
+            gradientW = gradientW + gw[:, np.newaxis]
 
         # This is here to make sure your gradient is the right shape
-        assert(w_grad.shape == (X.shape[1],1))
+        assert(gradientW.shape == (X.shape[1],1))
 
         # Take the update step in gradient descent
-        w = w - step_size*w_grad
+        w = w - step_size * gradientW
         
-        # Calculate the negative log-likelihood with the 
+        # Calculate the negative log-likelihood with the
         # new weight vector and store it for plotting later
         losses.append(calculateNegativeLogLikelihood(X,y,w))
+        t1 = time.time()
+        print("exe time {}", t1-t2)
         
     return w, losses
 
@@ -171,18 +180,19 @@ def trainLogistic(X,y, max_iters=max_iters, step_size=step_size):
 # Given an input data matrix X, add a column of ones to the left-hand
 # side
 #
-# Input: 
-#   X --   a n-by-d matrix of examples where each row
+# Input:
+#   X -- a n-by-d matrix of examples where each row
 #                   corresponds to a single d-dimensional example
 #
 # Output:
-#   aug_X --  a n-by-(d+1) matrix of examples where each row
+#   aug_X -- a n-by-(d+1) matrix of examples where each row
 #                   corresponds to a single d-dimensional example
 #                   where the the first column is all ones
 #
 ######################################################################
 def dummyAugment(X):
-  raise Exception('Student error: You haven\'t implemented dummyAugment yet.')
+    return np.hstack((np.ones((len(X),1)),X))
+  
 
 
 
@@ -194,7 +204,7 @@ def dummyAugment(X):
 
 # Given a matrix X (n x d) and y (n x 1), perform k fold cross val.
 def kFoldCrossVal(X, y, k):
-  fold_size = int(np.ceil(len(X)/k))
+  fold_size = int(np.ceil(len(X) / k))
   
   rand_inds = np.random.permutation(len(X))
   X = X[rand_inds]
@@ -204,10 +214,10 @@ def kFoldCrossVal(X, y, k):
   inds = np.arange(len(X))
   for j in range(k):
     
-    start = min(len(X),fold_size*j)
-    end = min(len(X),fold_size*(j+1))
+    start = min(len(X),fold_size * j)
+    end = min(len(X),fold_size * (j + 1))
     test_idx = np.arange(start, end)
-    train_idx = np.concatenate( [np.arange(0,start), np.arange(end, len(X))] )
+    train_idx = np.concatenate([np.arange(0,start), np.arange(end, len(X))])
     if len(test_idx) < 2:
       break
 
@@ -219,12 +229,13 @@ def kFoldCrossVal(X, y, k):
 
     w, losses = trainLogistic(X_fold_train, y_fold_train)
 
-    acc.append(np.mean((X_fold_test@w >= 0) == y_fold_test))
+    acc.append(np.mean((X_fold_test @ w >= 0) == y_fold_test))
 
   return np.mean(acc), np.std(acc)
 
 
-# Loads the train and test splits, passes back x/y for train and just x for test
+# Loads the train and test splits, passes back x/y for train and just x for
+# test
 def loadData():
   train = np.loadtxt("train_cancer.csv", delimiter=",")
   test = np.loadtxt("test_cancer_pub.csv", delimiter=",")
@@ -234,6 +245,5 @@ def loadData():
   X_test = test
   
   return X_train, y_train[:, np.newaxis], X_test   # The np.newaxis trick changes it from a (n,) matrix to a (n,1) matrix.
-
 
 main()
