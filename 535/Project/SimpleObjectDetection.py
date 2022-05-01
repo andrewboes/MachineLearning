fb8adad262598f9746588b0a18e87fd4785086c4
Andrew <andrew.boes@gmail.com>
2022-05-01 13:57:21 -0700

    Pretty good detector. Lots of false negatives, not a lot of false positives.

--- 535/Project/SimpleObjectDetection.py
+++ 535/Project/SimpleObjectDetection.py
@@ -2,6 +2,7 @@ import cv2 #pip install opencv-python
 #import matplotlib.image as img
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 #import torch
 
 def parseVideo():
@@ -16,42 +17,52 @@ def parseVideo():
 
 def main():
   #parseVideo()
-  for i in range(1):
-    img = cv2.imread("./frames/frame%d.jpg" %(43))
+  for i in range(7200):
+    print(i)
+    image = cv2.imread("./frames/frame%d.jpg" %(i))
     
-
-
-    # Read the original image
-    #img = cv2.imread('test.jpg') 
-    # Display original image
-    cv2.imshow('Original', img)
-    cv2.waitKey(0)
+    #img = cv2.imread("digitbox.jpg", 0)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    # Convert to graycsale
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Blur the image for better edge detection
-    img_blur = cv2.GaussianBlur(img_gray, (3,3), 0) 
+    mask = np.ones_like(img) * 255
+    boxes = []
     
-    # Sobel Edge Detection
-    sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
-    sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
-    sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
-    # Display Sobel Edge Detection Images
-    cv2.imshow('Sobel X', sobelx)
-    cv2.waitKey(0)
-    cv2.imshow('Sobel Y', sobely)
-    cv2.waitKey(0)
-    cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
-    cv2.waitKey(0)
+    for contour in contours:
+        if cv2.contourArea(contour) > 100:
+            hull = cv2.convexHull(contour)
+            cv2.drawContours(mask, [hull], -1, 0, -1)
+            x,y,w,h = cv2.boundingRect(contour)
+            boxes.append((x,y,w,h))
     
-    # Canny Edge Detection
-    edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
-    # Display Canny Edge Detection Image
-    cv2.imshow('Canny Edge Detection', edges)
-    cv2.waitKey(0)
+    boxes = sorted(boxes, key=lambda box: box[0])
     
-    cv2.destroyAllWindows()
-
+    mask = cv2.dilate(mask, np.ones((5,5),np.uint8))
+    
+    img[mask != 0] = 255
+    
+    result = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    showImage = False
+    
+    for n,box in enumerate(boxes):
+        x,y,w,h = box
+        diag = math.sqrt(w**2+h**2)
+        if diag > 170 and h > 50 and w < 1150:
+          showImage = True
+          print(diag)
+          print(h)
+          print(w)
+        cv2.rectangle(result,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.putText(result, str(n),(x+5,y+17), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255,0,0),2,cv2.LINE_AA)
+    
+    if showImage:
+      cv2.imshow("resut", result)
+      cv2.waitKey(0)
+    
+    #cv2.imwrite('digitbox_result.png', result)
+    
+  cv2.destroyAllWindows()
     
 if __name__=="__main__":
   main()
