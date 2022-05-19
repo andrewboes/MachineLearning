e90f096ac4b793fbea9c1bb556c1a06edc587d8e
Andrew <andrew.boes@gmail.com>
2022-05-19 14:22:21 -0700

    Made to work with boat data
    Changed rect values from float to int

--- 535/Project/MOT_tutorial.py
+++ 535/Project/MOT_tutorial.py
@@ -17,9 +17,10 @@ from sort import *
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE" #hack
 
-mot = "./MOT/"   # a custom path. you can change if you want to
+mot = "./MOT_boat/"   # a custom path. you can change if you want to
 MOT_PATH = mot
-motdata = MOT_PATH + 'train/MOT17-09/img1/'
+#motdata = MOT_PATH + 'train/MOT17-09/img1/' #people
+motdata = MOT_PATH + 'data/' #boats
 COCO_INSTANCE_CATEGORY_NAMES = [
   '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
   'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
@@ -35,31 +36,28 @@ COCO_INSTANCE_CATEGORY_NAMES = [
   'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
 def get_prediction(img_path, threshold, model):
+  print(img_path)
   img = Image.open(img_path) # Load the image
   transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
   img = transform(img) # Apply the transform to the image
   pred = model([img]) # Pass the image to the model
   pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
-  pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+  pred_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
   pred_score = list(pred[0]['scores'].detach().numpy())
   pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
   pred_boxes = pred_boxes[:pred_t+1]
   pred_class = pred_class[:pred_t+1]
   return pred_boxes, pred_class
 
-def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=1.5, text_th=3):
+def object_detection_api(img_path, model, threshold=0.5, rect_th=3, text_size=1.5, text_th=3):
  
-  boxes, pred_cls = get_prediction(img_path, threshold) # Get predictions
+  boxes, pred_cls = get_prediction(img_path, threshold, model) # Get predictions
   img = cv2.imread(img_path) # Read image with cv2
   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
   for i in range(len(boxes)):
     cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
     cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
-  plt.figure(figsize=(15,20)) # display the output image
-  plt.imshow(img)
-  plt.xticks([])
-  plt.yticks([])
-  plt.show()
+  cv2.imwrite("./MOT_boat/detect.jpg", img)   
   
 def main():  
   list_motdata = os.listdir(motdata)  
@@ -76,6 +74,8 @@ def main():
   model.eval()
 
   print(skimage.__version__)
+  
+  object_detection_api(img_ex_path, model,threshold=0.8)
   
   jsonpath = MOT_PATH + '3_MOT_detinfo.json'
 
@@ -100,7 +100,7 @@ def main():
         scores = info['scores']
         templist = bbox+[scores]
         
-        if labels == 1: # label 1 is a person in MS COCO Dataset
+        if labels == 9: # label 1 is a person in MS COCO Dataset, 9 is boat
             arrlist.append(templist)
             
     track_bbs_ids = mot_tracker.update(np.array(arrlist))
