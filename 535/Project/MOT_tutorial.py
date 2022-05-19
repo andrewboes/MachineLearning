e5d5273cb7cd8d73465b3da7e1b50e7181f88579
Andrew <andrew.boes@gmail.com>
2022-05-19 14:23:16 -0700

    Fixed issue where unclassed images where throwing null ref
    Added references

--- 535/Project/MOT_tutorial.py
+++ 535/Project/MOT_tutorial.py
@@ -1,3 +1,8 @@
+#references:
+#https://colab.research.google.com/github/mlvlab/COSE474/blob/master/3_Object_Detection_and_MOT_tutorial.ipynb#scrollTo=cnS6Q7M7C2VP
+#https://learnopencv.com/faster-r-cnn-object-detection-with-pytorch/
+#https://towardsdatascience.com/object-detection-and-tracking-in-pytorch-b3cf1a696a98
+
 import os 
 #from os.path import join
 #root = '/mot/drive/'
@@ -44,20 +49,22 @@ def get_prediction(img_path, threshold, model):
   pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
   pred_boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
   pred_score = list(pred[0]['scores'].detach().numpy())
-  pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
-  pred_boxes = pred_boxes[:pred_t+1]
-  pred_class = pred_class[:pred_t+1]
-  return pred_boxes, pred_class
+  if pred_score:
+    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+    pred_boxes = pred_boxes[:pred_t+1]
+    pred_class = pred_class[:pred_t+1]
+    return pred_boxes, pred_class
+  return
 
 def object_detection_api(img_path, model, threshold=0.5, rect_th=3, text_size=1.5, text_th=3):
- 
   boxes, pred_cls = get_prediction(img_path, threshold, model) # Get predictions
-  img = cv2.imread(img_path) # Read image with cv2
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
-  for i in range(len(boxes)):
-    cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
-    cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
-  cv2.imwrite("./MOT_boat/detect.jpg", img)   
+  if boxes:
+    img = cv2.imread(img_path) # Read image with cv2
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+    for i in range(len(boxes)):
+      cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
+      cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
+    cv2.imwrite("./MOT_boat/detect.jpg", img)   
   
 def main():  
   list_motdata = os.listdir(motdata)  
@@ -67,15 +74,12 @@ def main():
   img_ex_origin = cv2.imread(img_ex_path)
   img_ex = cv2.cvtColor(img_ex_origin, cv2.COLOR_BGR2RGB)
   
-  plt.imshow(img_ex)
-  plt.axis('off')
-  plt.show()
   model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
   model.eval()
 
   print(skimage.__version__)
   
-  object_detection_api(img_ex_path, model,threshold=0.8)
+  object_detection_api(img_ex_path, model, threshold=0.8)
   
   jsonpath = MOT_PATH + '3_MOT_detinfo.json'
 
