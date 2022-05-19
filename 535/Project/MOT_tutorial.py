a91dc569ebe5622ca0d0a15e64daed1fcc7de241
Andrew <andrew.boes@gmail.com>
2022-05-19 13:31:23 -0700

    Create MOT_tutorial.py

---
+++ 535/Project/MOT_tutorial.py
@@ -0,0 +1,128 @@
+import os 
+#from os.path import join
+#root = '/mot/drive/'
+from PIL import Image
+import numpy as np
+import torch
+import torchvision
+from torchvision import transforms as T
+import matplotlib.pylab as plt
+import cv2
+import sys
+import skimage
+import json
+import collections
+from pprint import pprint
+from sort import *
+
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE" #hack
+
+mot = "./MOT/"   # a custom path. you can change if you want to
+MOT_PATH = mot
+motdata = MOT_PATH + 'train/MOT17-09/img1/'
+COCO_INSTANCE_CATEGORY_NAMES = [
+  '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+  'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+  'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+  'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+  'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+  'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+  'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+  'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+  'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+  'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+  'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+  'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+def get_prediction(img_path, threshold, model):
+  img = Image.open(img_path) # Load the image
+  transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+  img = transform(img) # Apply the transform to the image
+  pred = model([img]) # Pass the image to the model
+  pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+  pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+  pred_score = list(pred[0]['scores'].detach().numpy())
+  pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+  pred_boxes = pred_boxes[:pred_t+1]
+  pred_class = pred_class[:pred_t+1]
+  return pred_boxes, pred_class
+
+def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=1.5, text_th=3):
+ 
+  boxes, pred_cls = get_prediction(img_path, threshold) # Get predictions
+  img = cv2.imread(img_path) # Read image with cv2
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+  for i in range(len(boxes)):
+    cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
+    cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
+  plt.figure(figsize=(15,20)) # display the output image
+  plt.imshow(img)
+  plt.xticks([])
+  plt.yticks([])
+  plt.show()
+  
+def main():  
+  list_motdata = os.listdir(motdata)  
+  list_motdata.sort()
+  
+  img_ex_path = motdata + list_motdata[0]
+  img_ex_origin = cv2.imread(img_ex_path)
+  img_ex = cv2.cvtColor(img_ex_origin, cv2.COLOR_BGR2RGB)
+  
+  plt.imshow(img_ex)
+  plt.axis('off')
+  plt.show()
+  model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+  model.eval()
+
+  print(skimage.__version__)
+  
+  jsonpath = MOT_PATH + '3_MOT_detinfo.json'
+
+  with open(jsonpath) as data_file:    
+     data = json.load(data_file)
+  odata = collections.OrderedDict(sorted(data.items()))
+  pprint(odata)
+  img_path = motdata    # img root path
+
+# Making new directory for saving results
+  save_path = MOT_PATH + 'save/'
+  mot_tracker = Sort() 
+  for key in odata.keys():   
+    arrlist = []
+    det_img = cv2.imread(os.path.join(img_path, key))
+    overlay = det_img.copy()
+    det_result = data[key] 
+    
+    for info in det_result:
+        bbox = info['bbox']
+        labels = info['labels']
+        scores = info['scores']
+        templist = bbox+[scores]
+        
+        if labels == 1: # label 1 is a person in MS COCO Dataset
+            arrlist.append(templist)
+            
+    track_bbs_ids = mot_tracker.update(np.array(arrlist))
+    
+    mot_imgid = key.replace('.jpg','')
+    newname = save_path + mot_imgid + '_mot.jpg'
+    print(mot_imgid)
+    
+    for j in range(track_bbs_ids.shape[0]):  
+        ele = track_bbs_ids[j, :]
+        x = int(ele[0])
+        y = int(ele[1])
+        x2 = int(ele[2])
+        y2 = int(ele[3])
+        track_label = str(int(ele[4])) 
+        cv2.rectangle(det_img, (x, y), (x2, y2), (0, 255, 255), 4)
+        cv2.putText(det_img, '#'+track_label, (x+5, y-10), 0,0.6,(0,255,255),thickness=2)
+        
+    cv2.imwrite(newname,det_img)
+
+  
+
+if __name__=="__main__":
+  main()
+
