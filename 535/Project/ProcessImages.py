79dde039a6dd36c6ed97338ef166bd44666402fd
Andrew <andrew.boes@gmail.com>
2022-05-25 10:30:07 -0700

    Removed commented out code

--- 535/Project/ProcessImages.py
+++ 535/Project/ProcessImages.py
@@ -5,7 +5,6 @@ import numpy as np
 import math
 from PIL import Image
 import torchvision
-
 from datetime import datetime
 from torchvision import transforms as T
 import os
@@ -71,21 +70,10 @@ def classifyFile(file, model, jsonDir, boatDir, noBoatDir):
         shutil.copyfile(file, boatDir+fileName+'.jpg')
   if not copied:
     shutil.copyfile(file, noBoatDir+fileName+'.jpg')
-    
-  
-        
-  
   with open(jsonDir + fileName + '.json', 'w') as fp:
     json.dump(output, fp)  
   return fileName, output
     
-    
-  
-# =============================================================================
-#   with open(jsonpath, 'w') as fp:
-#     json.dump(box_dictionary, fp)  
-# =============================================================================
-
 def main():
   jsonDirectory = 'json/'
   withBoatDirectory = 'boat/'
@@ -125,80 +113,7 @@ def main():
       directoryOutput[fileName + ".jpg"] = output
     with open(j+'directoryOutput.json', 'w') as fp:
       json.dump(directoryOutput, fp)  
-      
-     
-        
-# =============================================================================
-#   for _, directory, _ in os.walk(folder): #get list of folders
-#     for _,_,files in os.walk(directory):
-# =============================================================================
-    
-  
-  
-  
-# =============================================================================
-#     for directory in dirs:#loop through folders
-#       print(dirs)
-#       for _,_,files in os.walk(directory):
-#         for file in files:
-#           classifyFile(file, model)
-# =============================================================================
-      
-    #loop through files
-      #classify images
-      #save classification in json
-      #save "water" files in one folder
-      #save "boat" files in another
-# =============================================================================
-#   for i in range(7200):
-#     print(i)
-#     image = cv2.imread("./frames/frame%d.jpg" %(i))
-#     
-#     #img = cv2.imread("digitbox.jpg", 0)
-#     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     _, thresh = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
-#     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     
-#     mask = np.ones_like(img) * 255
-#     boxes = []
-#     
-#     for contour in contours:
-#         if cv2.contourArea(contour) > 100:
-#             hull = cv2.convexHull(contour)
-#             cv2.drawContours(mask, [hull], -1, 0, -1)
-#             x,y,w,h = cv2.boundingRect(contour)
-#             boxes.append((x,y,w,h))
-#     
-#     boxes = sorted(boxes, key=lambda box: box[0])
-#     
-#     mask = cv2.dilate(mask, np.ones((5,5),np.uint8))
-#     
-#     img[mask != 0] = 255
-#     
-#     result = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-#     showImage = True
-#     
-#     for n,box in enumerate(boxes):
-#         x,y,w,h = box
-#         diag = math.sqrt(w**2+h**2)
-# # =============================================================================
-# #         if diag > 170 and h > 50 and w < 1150:
-# #           showImage = True
-# #           print(diag)
-# #           print(h)
-# #           print(w)
-# # =============================================================================
-#         cv2.rectangle(result,(x,y),(x+w,y+h),(255,0,0),2)
-#         cv2.putText(result, str(n),(x+5,y+17), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255,0,0),2,cv2.LINE_AA)
-#     
-#     if showImage:
-#       cv2.imshow("resut", result)
-#       cv2.waitKey(0)
-#     
-#     #cv2.imwrite('digitbox_result.png', result)
-#     
-#   cv2.destroyAllWindows()
-# =============================================================================
+
     
 if __name__=="__main__":
   main()
